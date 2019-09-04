12170ef82eaa6ef2822d35ba1820f77d660bcb73
Angel <you@example>
2019-09-04 16:12:42 +0800

    新增切換寵物 view 完成體重紀錄頁面

--- PetNote/Controller/Profile/WeightRecordViewController.swift
+++ PetNote/Controller/Profile/WeightRecordViewController.swift
@@ -13,8 +13,6 @@ class WeightRecordViewController: BaseViewController {
 
     @IBOutlet weak var userSwitchLayer: UIView!
     
-    @IBOutlet weak var chartLayer: UIView!
-//    @IBOutlet weak var noteLayer: UIView!
     
     @IBOutlet weak var tableView: UITableView! {
         didSet {
@@ -27,7 +25,7 @@ class WeightRecordViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setChartView()
+        setupPetSwitchLayer()
         setupTableView()
     }
     
@@ -35,15 +33,20 @@ class WeightRecordViewController: BaseViewController {
         tableView.registerCellWithNib(identifier: String(describing: ChartTableViewCell.self), bundle: nil)
         tableView.registerCellWithNib(identifier: String(describing: WeightLabelTableViewCell.self), bundle: nil)
     }
-    
-    func setChartView() {
-        let chartViewFrame = CGRect(x: 0, y: 0, width: chartLayer.frame.width, height: chartLayer.frame.height)
-        let chartView = PNChartView(chartViewFrame)
+
+    func setupPetSwitchLayer() {
+        let switchPetView = SwitchPetView(frame: CGRect(x: 0, y: 0,
+                                                        width: userSwitchLayer.frame.width,
+                                                        height: userSwitchLayer.frame.height))
+        userSwitchLayer.addSubview(switchPetView)
         
-        chartLayer.addSubview(chartView)
-        chartView.setup()
     }
-    
+}
+
+extension WeightRecordViewController: SwitchPetViewDelegate {
+    func changePet() {
+        // TODO:
+    }
 }
 
 extension WeightRecordViewController: UITableViewDelegate {
---
+++ PetNote/View/Basic/SwitchPetView.swift
@@ -0,0 +1,90 @@
+//
+//  SwitchPetView.swift
+//  PetNote
+//
+//  Created by iching chen on 2019/9/4.
+//  Copyright © 2019 ichingchen. All rights reserved.
+//
+
+import UIKit
+protocol SwitchPetViewDelegate: AnyObject {
+    func changePet()
+}
+
+class SwitchPetView: UIView {
+
+    var collectionView: UICollectionView! {
+        
+        didSet {
+            
+            collectionView.dataSource = self
+            
+            collectionView.delegate = self
+        }
+    }
+    
+    weak var delegate: SwitchPetViewDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupCollectionView()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCollectionView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCollectionView() {
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10) // section與section之間的距離(如果只有一個section，可以想像成frame)
+        layout.itemSize = CGSize(width: 80, height: 80) // cell的寬、高
+        layout.minimumLineSpacing = CGFloat(integerLiteral: 10) // 滑動方向為「垂直」的話即「上下」的間距;滑動方向為「平行」則為「左右」的間距
+        layout.minimumInteritemSpacing = CGFloat(integerLiteral: 10) // 滑動方向為「垂直」的話即「左右」的間距;滑動方向為「平行」則為「上下」的間距
+        layout.scrollDirection = UICollectionView.ScrollDirection.horizontal // 滑動方向預設為垂直。注意若設為垂直，則cell的加入方式為由左至右，滿了才會換行；若是水平則由上往下，滿了才會換列
+        
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height), collectionViewLayout: layout)
+        print("aaaa")
+        self.addSubview(collectionView)
+        collectionView.backgroundColor = .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        collectionView.topAnchor.constraint(equalTo: self.topAnchor),
+        collectionView.leftAnchor.constraint(equalTo: self.leftAnchor),
+        collectionView.rightAnchor.constraint(equalTo: self.rightAnchor),
+        collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        
+        collectionView.registerCellWithNib(
+            identifier: String(describing: PetsCollectionViewCell.self),
+            bundle: nil
+        )
+    }
+}
+
+extension SwitchPetView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        delegate?.changePet()
+    }
+}
+
+extension SwitchPetView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PetsCollectionViewCell.identifier, for: indexPath) as? PetsCollectionViewCell
+            else {
+                return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+}
