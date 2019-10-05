55f5f99b77fe0207191061b16ee8563f431b5f2c
Angel <you@example>
2019-10-05 10:54:19 +0800

    添加地圖圖釘資訊框

--- PetNote/Controller/Other/MapViewController.swift
+++ PetNote/Controller/Other/MapViewController.swift
@@ -45,11 +45,13 @@ class MapViewController: BaseViewController {
             zoom: 7.3
         )
         mapView.camera = camera
+        // 顯示使用者位置按鈕
+//        mapView.settings.myLocationButton = true
 //        mapView.isMyLocationEnabled = true // 開啟我的位置(藍色小點)
         mapView.delegate = self
     }
     
-    func changeGMSCamera(latitude: Double, longitude: Double, zoom: Float = 16) {
+    func changeGMSCamera(latitude: Double, longitude: Double, zoom: Float = 15) {
         let camera = GMSCameraPosition.camera(
             withLatitude: latitude,
             longitude: longitude,
@@ -58,45 +60,44 @@ class MapViewController: BaseViewController {
         mapView.camera = camera
     }
     
-    // 查詢要顯示的醫院座標
     func setLandMarker(from hospitals: [Hospital]) {
-                
+        
+        // 查詢要顯示的醫院座標
         hospitals.forEach { hospital in
             let address = hospital.address
             group.enter()
-            print("====== enter")
             getLocationTask.async(group: group, qos: .default) {
+                // 查詢 core data 中是否已有儲存座標資訊
                 StorageManager.shared.featchHospitals(address: address) {[weak self] result in
                     switch result {
                     case .success(let hospitals):
                         self?.pnHospitals.append(hospitals[0])
-//                        self?.createMarker(pnHospital: hospitals[0])
-//                        self?.createMarker(pnHospital: hospitals[0])
-                        print("===(1)leave \(hospitals[0].name)")
                         self?.group.leave()
+                        
                     case .failure:
-                        print("(2)")
+                        // 若無則透過 CoreLocation 查詢座標資訊
                         self?.getLocationWithCoreLocation(hospital)
                     }
                 }
             }
         }
         
+        // 將所有座標製作圖釘顯示在地圖上
         group.notify(queue: .main) { [weak self] in
             guard let pnHospitals = self?.pnHospitals else { return }
             self?.createMarker(pnHospitals: pnHospitals)
         }
     }
     
-    // 用地址查詢座標
+    // 處理地址轉座標回傳結果
     func getLocationWithCoreLocation(_ hospital: Hospital) {
+        
         getLocation(address: hospital.address) {[weak self] result in
             switch result {
             case .success(let placeMark):
                 guard
                     let location = placeMark.location?.coordinate
                 else {
-                    print("===(2)leave (location = nil)")
                     self?.group.leave()
                     return
                 }
@@ -111,16 +112,13 @@ class MapViewController: BaseViewController {
                     longitude: location.longitude
                 )
                 
-//                StorageManager.shared.saveAll()
+                StorageManager.shared.saveAll()
                 
                 self?.pnHospitals.append(pnHospitalObject)
-//                self?.createMarker(pnHospital: pnHospitalObject)
-//                self?.createMarker(pnHospital: pnHospitalObject)
-                print("===(2)leave \(pnHospitalObject.name)")
                 self?.group.leave()
+                
             case .failure(let error):
                 print(error)
-                print("===(2)leave (nil)")
                 self?.group.leave()
                 return
             }
@@ -130,6 +128,7 @@ class MapViewController: BaseViewController {
     // 用 CoreLocation 把地址轉座標
     func getLocation(address: String,
                      completionHandler: @escaping (Result<CLPlacemark, Error>) -> Void) {
+        
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(address) { placemarks, error in
             
@@ -147,9 +146,6 @@ class MapViewController: BaseViewController {
                 return
             }
             
-            let lat = location.coordinate.latitude
-            let lon = location.coordinate.longitude
-            print("Lat: \(lat), Lon: \(lon)")
             completionHandler(Result.success(placemark))
             
         }
@@ -168,7 +164,6 @@ class MapViewController: BaseViewController {
     }
     
     func createMarker(pnHospitals: [PNHospital]) {
-        print("========= create")
         markers = []
         pnHospitals.forEach {
             let marker = createMarker(pnHospital: $0)
@@ -187,20 +182,45 @@ extension MapViewController: GMSMapViewDelegate {
     // 回傳點擊後要顯示的 View
     func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
         
-        // TODO: prepar markerInfo view
+        // prepar markerInfo view
         let view = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 100))
-        view.backgroundColor = .systemYellow
-        let title = UILabel(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: view.frame.size))
-        title.text = marker.title
-        view.addSubview(title)
+        view.backgroundColor = UIColor.pnBlueDark
+        view.addCorner(cornerRadius: 3)
+        
+        let titleLabel = UILabel()
+        titleLabel.text = marker.title
+        titleLabel.textColor = .white
+        
+        let discribeLabel = UILabel()
+        discribeLabel.text = "點擊規劃路線"
+        discribeLabel.textColor = .white
+        discribeLabel.font = discribeLabel.font.withSize(14)
+        
+        view.addSubview(titleLabel)
+        view.addSubview(discribeLabel)
+               
+        view.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        discribeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 5),
+            titleLabel.bottomAnchor.constraint(equalTo: discribeLabel.topAnchor, constant: -3),
+            titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -8),
+            titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8),
+            discribeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            discribeLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -5)
+        ])
         
         return view
         
     }
     
+    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+        print(marker.position)
+    }
+    
     func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
-        // TODO: 依據選取的 marker 顯示資料
-        
         return false
     }
 }
